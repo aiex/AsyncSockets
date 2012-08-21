43789ed7c08f343c59f95f0752fa8bfa59d695bc
Oscar Nygren <o.nygren@gmail.com>
2012-08-21 21:37:38 +0200

    Add ToString() for Net.Address

--- AsyncSockets/Net/Address.cs
+++ AsyncSockets/Net/Address.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AddressFamily = System.Net.Sockets.AddressFamily;
+
 namespace AsyncSockets.Net
 {
     public class Address
@@ -11,5 +13,13 @@ namespace AsyncSockets.Net
         public string IP { get; set; }
         public int Port { get; set; }
         public string Family { get; set; }
+
+        public override string ToString()
+        {
+            if (Family == AddressFamily.InterNetworkV6.ToString())
+                return string.Format("[{0}]:{1}", IP, Port);
+            else
+                return string.Format("{0}:{1}", IP, Port);
+        }
     }
 }
